4c647729d08d03b090ac11df122b622079adf7cc
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-27 16:43:13 +0300

    add class Medicine

---
+++ Lesson22-Pharmacy/Medicine.cpp
@@ -0,0 +1,53 @@
+#include "Medicine.h"
+
+Medicine::Medicine()
+{
+}
+
+Medicine::Medicine(const char* title, TYPES type, const char* country, float price)
+{
+}
+
+Medicine::~Medicine()
+{
+}
+
+void Medicine::setTitle(const char* title)
+{
+}
+
+void Medicine::setType(TYPES type)
+{
+}
+
+void Medicine::setCountry(const char* country)
+{
+}
+
+void Medicine::setPrice(float price)
+{
+}
+
+const char* Medicine::getTitle() const
+{
+    return nullptr;
+}
+
+TYPES Medicine::getType() const
+{
+    return TYPES();
+}
+
+const char* Medicine::getCountry() const
+{
+    return nullptr;
+}
+
+float Medicine::getPrice() const
+{
+    return 0.0f;
+}
+
+void Medicine::showInfo() const
+{
+}
---
+++ Lesson22-Pharmacy/Medicine.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+enum TYPES {
+	other,
+	pills,
+	capsules,
+	syrope
+};
+
+class Medicine
+{
+	char* m_title;
+	TYPES m_type;
+	char* m_country;
+	float m_price;
+public:
+	Medicine();
+	Medicine(const char* title, TYPES type, const char* country,
+		float price);
+	~Medicine();
+
+	void setTitle(const char* title);
+	void setType(TYPES type);
+	void setCountry(const char* country);
+	void setPrice(float price);
+
+	const char* getTitle()const;
+	TYPES getType()const;
+	const char* getCountry()const;
+	float getPrice()const;
+
+	void showInfo()const;
+};
+
