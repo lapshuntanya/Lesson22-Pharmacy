352e4c59bebcc37cc08fc7345eaa1219a33138bc
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-27 17:11:34 +0300

    main

--- Lesson22-Pharmacy/Lesson22-Pharmacy.cpp
+++ Lesson22-Pharmacy/Lesson22-Pharmacy.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include "Medicine.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Medicine a;
+	Medicine b("Aspirin", TYPES::pills, "India", 20.99);
+	Medicine c("Nurofen", TYPES::capsules, "Italy", 34.56);
+	const Medicine d("Dr. MOM", TYPES::syrope, "France", 150.70);
+
+	a.showInfo();
+	b.showInfo();
+	c.showInfo();
+	d.showInfo();
+
 }
 
