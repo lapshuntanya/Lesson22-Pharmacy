232b4eccc2776d7ffc118afaabda304b60301a20
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-27 17:01:10 +0300

    setters

--- Lesson22-Pharmacy/Medicine.cpp
+++ Lesson22-Pharmacy/Medicine.cpp
@@ -1,31 +1,60 @@
 #include "Medicine.h"
 
-Medicine::Medicine()
-{
+Medicine::Medicine(){
+    m_title = new char[10] {"undefined"};
+    m_type = TYPES::other;
+    m_country = new char[8] {"Ukraine"};
+    m_price = 0;
 }
 
 Medicine::Medicine(const char* title, TYPES type, const char* country, float price)
 {
+    int sizeT = strlen(title) + 1; //Рахуємо кількість символів
+    m_title = new char[sizeT];//Виділяєму дин. пам'ять необх. розміру
+    strcpy_s(m_title, sizeT, title); //Копіюємо символи
+
+    m_type = type;
+
+    int sizeC = strlen(country) + 1; //Рахуємо кількість символів
+    m_country = new char[sizeC];//Виділяєму дин. пам'ять необх. розміру
+    strcpy_s(m_country, sizeC, country); //Копіюємо символи
+
+    m_price = price;
 }
 
-Medicine::~Medicine()
-{
+Medicine::~Medicine(){
+    delete[] m_title;
+    delete[] m_country;
 }
 
-void Medicine::setTitle(const char* title)
-{
+void Medicine::setTitle(const char* title){
+    if (strlen(title) >= 2)    {
+        delete[] m_title;
+
+        int sizeT = strlen(title) + 1; //Рахуємо кількість символів
+        m_title = new char[sizeT];//Виділяєму дин. пам'ять необх. розміру
+        strcpy_s(m_title, sizeT, title); //Копіюємо символи
+    }
 }
 
-void Medicine::setType(TYPES type)
-{
+void Medicine::setType(TYPES type){
+    m_type = type;
 }
 
-void Medicine::setCountry(const char* country)
-{
+void Medicine::setCountry(const char* country){
+    if (strlen(country) >= 2) {
+        delete[] m_country;
+
+        int sizeC = strlen(country) + 1; //Рахуємо кількість символів
+        m_country = new char[sizeC];//Виділяєму дин. пам'ять необх. розміру
+        strcpy_s(m_country, sizeC, country); //Копіюємо символи
+    }
 }
 
-void Medicine::setPrice(float price)
-{
+void Medicine::setPrice(float price){
+    if (price > 0) {
+        m_price = price;
+    }
 }
 
 const char* Medicine::getTitle() const
